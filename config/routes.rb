1f4f8edb1514249580a24677fdc54707e0e2dd4b
caroline assis <carolinemassis@gmail.com>
2016-08-01 13:11:38 +0100

    done rails with coach

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  root 'coaching#ask'
+
+  get 'coaching/answer'
+
+  get 'coaching/ask'
+
+  get 'answer' => 'coaching#answer'
+
+  get 'ask' => 'coaching#ask'
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
